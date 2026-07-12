fa4adda467a53a3381fb59f465c53599792d6bd9
far79 <mohammedfarhanahmed305@gmail.com>
2026-07-12 15:36:29 +0530

    Added area, circumference, diameter

--- formulas.py
+++ formulas.py
@@ -1,4 +1,12 @@
 import math
 
 def circle_area(radius):
-    return math.pi * radius * radius
+    return math.pi * radius ** 2
+
+
+def circle_circumference(radius):
+    return 2 * math.pi * radius
+
+
+def circle_diameter(radius):
+    return 2 * radius
--- main.py
+++ main.py
@@ -3,7 +3,11 @@ from kivy.lang import Builder
 from kivy.properties import StringProperty
 from kivy.uix.screenmanager import Screen
 
-from formulas import circle_area
+from formulas import (
+    circle_area,
+    circle_circumference,
+    circle_diameter
+)
 
 
 class HomeScreen(Screen):
@@ -13,13 +17,28 @@ class HomeScreen(Screen):
 class CalculatorScreen(Screen):
     result = StringProperty("")
 
-    def calculate(self):
+    def calculate_area(self):
         try:
             radius = float(self.ids.radius.text)
-            area = circle_area(radius)
-            self.result = f"Area = {area:.2f}"
+            self.result = f"Area = {circle_area(radius):.2f}"
         except ValueError:
-            self.result = "Please enter a valid number."
+            self.result = "Enter a valid number."
+
+    def calculate_circumference(self):
+        try:
+            radius = float(self.ids.radius.text)
+            self.result = (
+                f"Circumference = {circle_circumference(radius):.2f}"
+            )
+        except ValueError:
+            self.result = "Enter a valid number."
+
+    def calculate_diameter(self):
+        try:
+            radius = float(self.ids.radius.text)
+            self.result = f"Diameter = {circle_diameter(radius):.2f}"
+        except ValueError:
+            self.result = "Enter a valid number."
 
 
 class GeometryCalculatorApp(App):
